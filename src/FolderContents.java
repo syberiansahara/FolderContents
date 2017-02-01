5832064c2b68575111c05bab72003b75641b6c02
syberiansahara <ninefoldcomplex@gmail.com>
2017-02-02 01:55:42 +0300

    !!!!!!!!!

--- src/FolderContents.java
+++ src/FolderContents.java
@@ -1,37 +1,40 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class FolderContents {
 
-    static class DirectoryNotFoundException extends RuntimeException {
-        void printWarning() {
-            System.out.println("Directory does not exist.");
-        }
+    public static void main(String[] args) {
+        FolderContents folderContents = new FolderContents();
+        folderContents.run();
     }
 
-    static String readFolderPath() throws DirectoryNotFoundException {
-        System.out.println("This program lists all the files contained in a given folder. \n" +
-                "Please enter directory path to be looked through.");
-        Scanner thisScanner = new Scanner(System.in);
-        String theFolderPath = thisScanner.nextLine();
-        thisScanner.close();
-        if ( ! new File(theFolderPath).isDirectory()) {
-            throw new DirectoryNotFoundException();
-        }
-        System.out.println("\n");
-        return theFolderPath;
+    public void run() {
+        String theFolderPath = readFolderPath();
+        makeBuildList(theFolderPath);
     }
 
-    private static void makeBuildList(String theFolderPath) throws IOException {
-        try{
+    private String readFolderPath() {
+        return "c:\\Users\\syber\\Desktop\\";
+//        System.out.println("This program lists all the files contained in a given folder. \n" +
+//                "Please enter directory path to be looked through.");
+//        Scanner thisScanner = new Scanner(System.in);
+//        String theFolderPath = thisScanner.nextLine();
+//        thisScanner.close();
+//        if ( ! new File(theFolderPath).isDirectory()) {
+//            throw new RuntimeException("Directory does not exist");
+//        }
+//        System.out.println("\n");
+//        return theFolderPath;
+    }
+
+    private void makeBuildList(String theFolderPath) {
+        try (FileWriter outBuildList = new FileWriter(theFolderPath + "\\Folder_Contents.txt")) {
             final File theFolder = new File(theFolderPath);
-            FileWriter out = new FileWriter(theFolder.toString() + "\\build_list.txt");
-            //FileWriter out = new FileWriter("build_list.txt");
-
             Tier total = new Tier();
             Tier current = new Tier();
-
             Directory dir = new Directory(theFolder, current);
             total.add(dir.numberOfFiles() - 1);
             current.add(-1);
@@ -39,14 +42,13 @@ public class FolderContents {
             int i = 0;
             int k = 0;
 
-
             outerLoop:
-            while (current.last() < total.last() ) {
+            while (current.last() < total.last()) {
                 current.incrementLast();
 
                 if (dir.currentFile().isFile()) {
                     System.out.println(format(dir.currentFile(), theFolderPath));
-                    out.write(format(dir.currentFile(), theFolderPath) + "\r\n");
+                    outBuildList.write(format(dir.currentFile(), theFolderPath) + "\r\n");
                     i++;
                 } else if (dir.currentFile().isDirectory() &&
                         dir.numberOfFilesInCurrent() != 0) {
@@ -57,15 +59,10 @@ public class FolderContents {
                         dir.numberOfFilesInCurrent() == 0) {
                     System.out.println(format(dir.currentFile(), theFolderPath)
                             + " (Empty folder)");
-                    /*out.write(format(dir.currentFile(), theFolderPath)
-                            + " (Empty folder) \r\n"); */
                     k++;
                 }
 
-                //System.out.println(Arrays.toString(total.toArray()));
-                //System.out.println(Arrays.toString(current.toArray()));
-
-                while (current.last() == total.last()) {
+                while (current.last().equals(total.last())) {
                     if (total.size() != 1) {
                         dir = dir.levelUp();
                         total.cutLast();
@@ -74,24 +71,23 @@ public class FolderContents {
                     }
                 }
             }
-
-            out.close();
             System.out.println("Number of files: " + i + ", number of folders: " + k);
-
-        } catch (DirectoryNotFoundException e1) {
-            e1.printWarning();
-        } catch (NullPointerException e2) {
-            System.out.println("Directory is empty.");
+        } catch (IOException e) {
+            System.out.println("IOException when making FolderContents");
+            e.printStackTrace();
         }
     }
 
-    static String format(File file, String root) {
+    private String format(File file, String root) {
         return '.' + file.toString().substring(root.length()).replace('\\', '/');
     }
 
-    public static void main(String[] args) throws IOException {
-        String theFolderPath = readFolderPath();
-        makeBuildList(theFolderPath);
+    static class Tier extends ArrayList<Integer> {
+        Integer last() {return this.get(this.size() - 1);}
+
+        void cutLast() {this.remove(this.size() - 1);}
+
+        void incrementLast () {this.set(this.size() - 1, this.last() + 1);}
     }
 
     static class Directory {
@@ -123,13 +119,5 @@ public class FolderContents {
             current.add(-1);
             return newDir;
         }
-    }
-
-    static class Tier extends ArrayList<Integer> {
-        Integer last() {return this.get(this.size() - 1);}
-
-        void cutLast() {this.remove(this.size() - 1);}
-
-        void incrementLast () {this.set(this.size() - 1, this.last() + 1);}
     }
 }
